9438c9a174cb090f8760554d7e8867ddb6d793aa
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-11-23 16:13:57 +0000

    Add merge transcripts with stringtie smarttool runner

---
+++ smarttools/merge_transcripts_stringtie/smarttool_runner.py
@@ -0,0 +1,56 @@
+"""Merge transcript using stringtie."""
+
+import os
+
+from smarttoolbase import SmartTool, Command, parse_args
+
+
+BASE_COMMANDS = [
+    Command(
+        "stringtie --merge -G {gene_annotation_path} {mergelist_fpath} -o stringtie.gtf"
+    ),
+]
+OUTPUTS = ["stringtie.gtf"]
+
+class MergeTranscriptsStringtie(SmartTool):
+
+    def pre_run(self, identifier):
+
+        mergelist_fpath = os.path.join(self.working_directory, "mergelist.txt")
+        with open(mergelist_fpath, "w") as fh:
+            for i in self.input_dataset.identifiers:
+                item_fpath = self.input_dataset.item_content_abspath(i)
+                fh.write("{}\n".format(item_fpath))
+
+        self.base_command_props.update(
+            {'mergelist_fpath': mergelist_fpath}
+        )
+
+        self.base_command_props['gene_annotation_path'] = os.environ['GENE_ANNOTATION_PATH']  # NOQA
+
+    def stage_outputs(self, identifier):
+        for filename in self.outputs:
+
+            useful_name = ""
+
+            fpath = os.path.join(self.working_directory, filename)
+            relpath = os.path.join(useful_name, filename)
+            out_id = self.output_proto_dataset.put_item(fpath, relpath)
+            self.output_proto_dataset.add_item_metadata(
+                out_id,
+                'from',
+                "{}".format(self.input_dataset.uri)
+                )
+
+def main():
+    args = parse_args()
+
+    with MergeTranscriptsStringtie(args.input_uri, args.output_uri) as smart_tool:
+        smart_tool.base_commands = BASE_COMMANDS
+        smart_tool.outputs = OUTPUTS
+        smart_tool(args.identifier)
+
+
+
+if __name__ == '__main__':
+    main()
